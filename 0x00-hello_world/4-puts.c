69786cfe6c6042f1c02ec76298aa8b87067c3f2e
souhail <souhail.ouchfil@gmail.com>
2023-07-13 23:29:49 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 /**
  *main: prints "Programming is like building a multilingual puzzle
- *description value of 'main': Return: 0 if exited properly
  *
+ *
+ *description value of 'main': Return: 0 if exited properly
  */
 int main(void)
 {
