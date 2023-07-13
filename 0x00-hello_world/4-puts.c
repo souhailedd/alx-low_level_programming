1cabe1e0456498e2faddf6830beefd9fd96182a7
souhail <souhail.ouchfil@gmail.com>
2023-07-13 23:09:21 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- *description for function main: prints "Programming is like building a multilingual puzzle
+ *main: prints "Programming is like building a multilingual puzzle
  *description for return value of 'main': Return: 0 if exited properly
 */
 int main(void)
