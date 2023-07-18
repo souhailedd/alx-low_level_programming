41f0fdf88940771668f03fcb5533ce8c7f9ee7fc
souhail <souhail.ouchfil@gmail.com>
2023-07-18 21:08:17 +0000

    finnally

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
-// Custom putchar function to print a character
-int _putchar(char c) {
-  return putchar(c);
-}
-
-int main() {
-  // Call _putchar to print "_putchar"
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-
-  // Print a new line
-  _putchar('\n');
-
-  return (0);
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
